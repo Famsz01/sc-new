04206a3be5b0eca5d9c57bd5e7932000eefa8969
Famsz01 <azkam4988@gmail.com>
2022-03-05 01:19:07 +0700

    scnew

---
+++ shop/allpayment.js
@@ -0,0 +1,35 @@
+const allpayment = (prefix) => {
+return `PAYMENT BY FAMS
+
+*🏧 BANK*
+
+1. BRI 
+	ㅁ 1234 5678 9101 2345
+2. BCA 
+	ㅁ 1234 5678 9101 2345
+3. BNI 
+	ㅁ 1234 5678 9101 2345
+4. PERMATA
+	ㅁ 1234 5678 9101 2345
+	
+*💰 E-MONEY*
+
+1. GOPAY
+	ㅁ 0812 3456 7891
+2. OVO
+	ㅁ 0812 3456 7891
+3. DANA
+	ㅁ 0812 3456 7891
+	
+Semuanya atas nama Fams
+
+*📲 PULSA*
+
+Tidak tersedia
+	
+Sebelum melakukan pembayaran ada baiknya anda menghubungi owner terlebih dahulu!
+`
+	}
+
+exports.allpayment = allpayment
+ 
---
+++ shop/donasi.js
@@ -0,0 +1,29 @@
+const donasibot = () => {
+return `DONASI BISA VIA BANK / E-MONEY
+
+*🏧 BANK*
+
+1. BRI 
+	ㅁ 1234 5678 9101 2345
+2. BCA 
+	ㅁ 1234 5678 9101 2345
+3. BNI 
+	ㅁ 1234 5678 9101 2345
+4. PERMATA
+	ㅁ 1234 5678 9101 2345
+	
+*💰 E-MONEY*
+
+1. GOPAY
+	ㅁ 0812 3456 7891
+2. OVO
+	ㅁ 0812 3456 7891
+3. DANA
+	ㅁ 0812 3456 7891
+	
+Semuanya atas nama Fams
+`
+	}
+
+exports.donasibot = donasibot
+ 
